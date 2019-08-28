7320bc6a3d83d32b65f20ce67be907b0045600d9
flv <flv@blizzard.com>
2019-08-28 15:18:37 +0800

    fix syntax error

--- birthday_list_km.c
+++ birthday_list_km.c
@@ -3,18 +3,20 @@
 #include <linux/module.h>
 #include <linux/list.h>
 #include <linux/types.h>
+#include <linux/slab.h>
+
 struct birthday {
 	int day;
 	int month;
 	int year;
-	list_head list;
-}
+	struct list_head list;
+};
 
 static LIST_HEAD(birthday_list);
 
 int cjLinkedList_init(void){
 	printk(KERN_INFO "Creating Birthday list\n");
-	birthday *Kevin;
+	struct birthday *Kevin;
 	Kevin  = kmalloc(sizeof(*Kevin), GFP_KERNEL);
 	Kevin->day = 2;
 	Kevin->month = 8;
@@ -22,7 +24,7 @@ int cjLinkedList_init(void){
 	INIT_LIST_HEAD(&Kevin->list);
 	list_add_tail(&Kevin->list, &birthday_list);
 
-	birthday *David;
+	struct birthday *David;
 	David = kmalloc(sizeof(*David), GFP_KERNEL);
 	David->day = 1;
 	David->month = 6;
@@ -30,7 +32,7 @@ int cjLinkedList_init(void){
 	INIT_LIST_HEAD(&David->list);
 	list_add_tail(&David->list, &birthday_list);
 
-	birthday *Justin;
+    struct birthday *Justin;
 	Justin = kmalloc(sizeof(*Justin), GFP_KERNEL);
 	Justin->day = 3;
 	Justin->month = 7;
@@ -39,7 +41,7 @@ int cjLinkedList_init(void){
 	list_add_tail(&Justin->list, &birthday_list);
 
 
-	birthday *Andy;
+	struct birthday *Andy;
 	Andy = kmalloc(sizeof(*Andy), GFP_KERNEL);
 	Andy->day = 4;
 	Andy->month = 9;
@@ -47,7 +49,7 @@ int cjLinkedList_init(void){
 	INIT_LIST_HEAD(&Andy->list);
 	list_add_tail(&Andy->list, &birthday_list);
 
-	birthday *Sean;
+	struct birthday *Sean;
 	Sean = kmalloc(sizeof(*Sean), GFP_KERNEL);
 	Sean->day = 5;
 	Sean->month = 10;
@@ -56,26 +58,35 @@ int cjLinkedList_init(void){
 	list_add_tail(&Sean->list, &birthday_list);
 
 	printk(KERN_INFO "Traversing birthday list\n");
-	birthday *ptr;
+	struct birthday *ptr;
 	list_for_each_entry(ptr, &birthday_list, list){
 		printk(KERN_INFO "BIRTHDAY: Month: %d Day: %d Year: %d\n", ptr->month, ptr->day, ptr->year);
 	}
 	return 0;
 }
 
+/*
+void print_list(){
+	printk(KERN_INFO "Print Birthday list elements\n");
+	struct birthday *ptr, *next;
+	list_for_each_entry_safe(ptr, next, &birthday_list, list){
+		printk("%d-%d-%d\n", ptr->year, ptr->month, ptr->day);
+	}
+}
+*/
 
 void delete_list(void){
 	printk(KERN_INFO "Removing Birthday list elements\n");
-	birthday *ptr, *next;
+	struct birthday *ptr, *next;
 	list_for_each_entry_safe(ptr, next, &birthday_list, list){
 		list_del(&ptr->list);
 		kfree(ptr);
 	}
 }
 
-module_init(print_list);
+module_init(cjLinkedList_init);
 module_exit(delete_list);
 
-MODULE LICENSE("GPL"); 
-MODULE DESCRIPTION("Simple Module"); 
-MODULE AUTHOR("SGG");
+MODULE_LICENSE("GPL"); 
+MODULE_DESCRIPTION("Simple Module"); 
+MODULE_AUTHOR("SGG");
